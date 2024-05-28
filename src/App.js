401146f8ec79728d9da171777e17e110d379f35b
Solomon Chamamme Nnyabe <solomonchamamme@gmail.com>
2024-05-28 23:51:01 +0000

    Modified

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
-import React from 'react';
-import './App.css';
-import { BrowserRouter as Router, Route,Routes } from 'react-router-dom';
-import Home from './pages/Home'
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path='/' element={Home}/>
-      </Routes>
+      <div>
+        <Routes>
+          <Route path="/" element={<Home/>} />
+          <Route path="/about" element={<About/>} />
+        </Routes>
+      </div>
     </Router>
-)}
-
+  );
+}
 
 export default App;
